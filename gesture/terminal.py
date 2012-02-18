65264a67c3adc371b4d9c7b289bf2ccbe2ba084d
Richie Zeng <richzeng@gmail.com>
2012-02-18 09:34:56 -0800

    added terminal.py

---
+++ gesture/terminal.py
@@ -0,0 +1,13 @@
+import os
+
+def move_up():
+    os.system("./crikey '\A\t'")
+
+def move_down():
+    os.system("./crikey '\A\S\t'")
+
+def move_right():
+    os.system("./crikey '\C\t'")
+
+def move_left():
+    os.system("./crikey '\C\S\t'")
