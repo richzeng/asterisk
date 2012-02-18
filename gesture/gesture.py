1538b76b4691f9fd8c3a55c66f54a88f0392ceee
Richie Zeng <richzeng@gmail.com>
2012-02-18 09:30:22 -0800

    added terminal functionality to gesture.py

--- gesture/gesture.py
+++ gesture/gesture.py
@@ -1,4 +1,5 @@
 import mouse
+import terminal
 
 class Gesture():
     def __init__(self, name, waypoints, min_sizes = None, command = None, delay = 0.25):
@@ -163,10 +164,14 @@ def make_default_gesture(name, count, end, default_size, command=None):
 sample_points = [[(223, 189)], [(223, 187)], [(223, 184)], [(223, 182)], [(223, 179)], [(222, 176)], [(222, 173)], [(221, 170)], [(221, 167)], [(220, 164)], [(220, 161)], [(219, 158)], [(218, 155)], [(217, 151)], [(217, 148)], [(216, 145)], [(216, 142)], [(215, 138)], [(215, 135)], [(216, 132)], [(216, 129)], [(215, 125)], [(215, 122)], [(216, 120)], [(216, 117)], [(217, 113)], [(217, 110)], [(217, 107)], [(216, 103)], [(216, 99)], [(214, 95)], [(213, 92)], [(212, 89)], [(211, 87)], [(210, 87)], [(210, 89)]]
 
 one_up = Gesture("One Up", [[(0, 0), (0, -1)]], [(0, 50)])
-two_left = make_default_gesture("Two Left", 2, (-1, 0), (50, 0), mouse.move_left)
-two_right = make_default_gesture("Two Right", 2, (1, 0), (50, 0), mouse.move_right)
-two_up = make_default_gesture("Two Up", 2, (0, -1), (0, 50), mouse.move_up)
-two_down = make_default_gesture("Two Down", 2, (0, 1), (0, 50), mouse.move_down)
+#two_left = make_default_gesture("Two Left", 2, (-1, 0), (50, 0), mouse.move_left)
+#two_right = make_default_gesture("Two Right", 2, (1, 0), (50, 0), mouse.move_right)
+#two_up = make_default_gesture("Two Up", 2, (0, -1), (0, 50), mouse.move_up)
+#two_down = make_default_gesture("Two Down", 2, (0, 1), (0, 50), mouse.move_down)
+two_left = make_default_gesture("Two Left", 2, (-1, 0), (50, 0), terminal.move_left)
+two_right = make_default_gesture("Two Right", 2, (1, 0), (50, 0), terminal.move_right)
+two_up = make_default_gesture("Two Up", 2, (0, -1), (0, 50), terminal.move_up)
+two_down = make_default_gesture("Two Down", 2, (0, 1), (0, 50), terminal.move_down)
 two_together = Gesture("Two Together", [[(0, 0), (1, 0)], [(0, 0), (-1, 0)]], [(50, 0), (50, 0)])
 two_away = Gesture("Two Away", [[(0, 0), (-1, 0)], [(0, 0), (1, 0)]], [(50, 0), (50, 0)])
 gestures = [one_up, two_left, two_right, two_up, two_down, two_together, two_away]
