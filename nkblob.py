a78f69da8e20a66d99069c3a1a0cda67607f5e89
Nikita Kitaev <nikitakit@gmail.com>
2012-02-18 00:00:00 -0800

    New version of nkblob

--- nkblob.py
+++ nkblob.py
@@ -1,13 +1,12 @@
 import cv2
 from cv2 import cv
 
-window_hue = "Hue"
-window_rgb = "Image"
-
 class BlobTracker:
     def __init__(self):
-        cv.NamedWindow(window_hue,1)
-        cv.NamedWindow(window_rgb,1)
+        cv.NamedWindow("Hue",1)
+        cv.NamedWindow("Saturation",1)
+        cv.NamedWindow("Filtered",1)
+        cv.NamedWindow("Image",1)
         self.capture = cv.CaptureFromCAM(0)
 
     def run(self):
@@ -18,17 +17,41 @@ class BlobTracker:
             #cv.Dilate(img, img, None, 3)
             #cv.Erode(img, img, None, 1)
             #cv.Smooth(img, img, cv.CV_BLUR, 3)
-            img_H = cv.CreateImage(cv.GetSize(img), 8, 1)
-            img_S = cv.CreateImage(cv.GetSize(img), 8, 1)
-            img_V = cv.CreateImage(cv.GetSize(img), 8, 1)
-            cv.Split(img, img_H, img_S, img_V, None)
-            threshold = cv.CreateImage(cv.GetSize(img), 8, 1)
-            cv.Threshold(img_H, threshold, 15, 255, cv.CV_THRESH_BINARY)
-            cv.Merge(threshold, img_S, img_V, None, img)
+            h = cv.CreateImage(cv.GetSize(img), 8, 1)
+            s = cv.CreateImage(cv.GetSize(img), 8, 1)
+            v = cv.CreateImage(cv.GetSize(img), 8, 1)
+            cv.Split(img, h, s, v, None)
+            threshold_h = cv.CreateImage(cv.GetSize(img), 8, 1)
+            cv.Threshold(h, threshold_h, 15, 255, cv.CV_THRESH_BINARY)
+            cv.Merge(threshold_h, s, v, None, img)
             cv.CvtColor(img, img, cv.CV_HSV2BGR)
-            cv.ShowImage(window_hue, threshold)
-            cv.ShowImage(window_rgb, img)
+            cv.ShowImage("Hue", threshold_h)
+            cv.ShowImage("Image", img)
+
+    def run2(self):
+        while cv.WaitKey(10) != 27:
+            img = cv.QueryFrame(self.capture)
+            cv.Flip(img,img,1)
+            cv.CvtColor(img, img, cv.CV_BGR2HSV)
+            h = cv.CreateImage(cv.GetSize(img), 8, 1)
+            s = cv.CreateImage(cv.GetSize(img), 8, 1)
+            v = cv.CreateImage(cv.GetSize(img), 8, 1)
+            cv.Split(img, h, s, v, None)
+            threshold_h = cv.CreateImage(cv.GetSize(img), 8, 1)
+            threshold_s = cv.CreateImage(cv.GetSize(img), 8, 1)
+            threshold_total = cv.CreateImage(cv.GetSize(img), 8, 1)
+            cv.Threshold(h, threshold_h, 65, 255, cv.CV_THRESH_BINARY)
+            cv.Threshold(s, threshold_s, 125, 255, cv.CV_THRESH_BINARY)
+            cv.And(threshold_h, threshold_s, threshold_total)
+            #cv.Merge(threshold_h, s, v, None, img)
+            #cv.CvtColor(img, img, cv.CV_HSV2BGR)
+            #cv.ShowImage(window_hue, threshold_h)
+            cv.ShowImage("Hue", threshold_h)
+            cv.ShowImage("Saturation", threshold_s)
+            cv.ShowImage("Filtered", threshold_total)
+            cv.ShowImage("Image", img)
+            #cv.ShowImage(window_rgb, img)
 
 if __name__=="__main__":
     color_tracker = BlobTracker()
-    color_tracker.run()
+    color_tracker.run2()
