54e42e8f78028e09433101e636efe8818066e2c7
Mark Canning <argusdusty@gmail.com>
2012-02-18 08:58:40 -0800

    Fix - reverses direction to one_up

--- gesture/gesture.py
+++ gesture/gesture.py
@@ -92,7 +92,7 @@ def resize(gesture, points, min_size):
 
     return (ratio, x_shift, y_shift)
 
-THRESHOLD = 0.05
+THRESHOLD = 0.2
 
 def compare(gesture, points):
     finger_count = len(points[0])
@@ -159,7 +159,7 @@ def make_default_gesture(name, count, end, default_size, begin = (0, 0)):
     return Gesture(name, [[begin, end] for i in range(count)], [default_size for i in range(count)])
 
 sample_points = [[(223, 189)], [(223, 187)], [(223, 184)], [(223, 182)], [(223, 179)], [(222, 176)], [(222, 173)], [(221, 170)], [(221, 167)], [(220, 164)], [(220, 161)], [(219, 158)], [(218, 155)], [(217, 151)], [(217, 148)], [(216, 145)], [(216, 142)], [(215, 138)], [(215, 135)], [(216, 132)], [(216, 129)], [(215, 125)], [(215, 122)], [(216, 120)], [(216, 117)], [(217, 113)], [(217, 110)], [(217, 107)], [(216, 103)], [(216, 99)], [(214, 95)], [(213, 92)], [(212, 89)], [(211, 87)], [(210, 87)], [(210, 89)]]
-one_up = Gesture("One Up", [[(0, 0), (0, 1)]], [(0, 50)])
+one_up = Gesture("One Up", [[(0, 0), (0, -1)]], [(0, 50)])
 two_left = make_default_gesture("Two Left", 2, (-1, 0), (50, 0))
 two_right = make_default_gesture("Two Right", 2, (1, 0), (50, 0))
 two_up = make_default_gesture("Two Up", 2, (0, -1), (0, 50))
