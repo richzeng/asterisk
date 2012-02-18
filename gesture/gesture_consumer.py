dad81cc4fbc70f9029386e1ae2c130df0fcb1482
Nikita Kitaev <nikitakit@gmail.com>
2012-02-18 07:10:32 -0800

    Integrate gestures into consumer/producer framework

---
+++ gesture/gesture_consumer.py
@@ -0,0 +1,28 @@
+from gesture import *
+
+import sys, os
+sys.path.insert(0, os.path.abspath('..'))
+import tracking
+from tracking import util
+
+def gesture_consumer(to):
+    MAX_POINTS = 150
+    most_recent = 0
+    while True:
+        to.flush()
+        if len(to) > MAX_POINTS:
+            del to[:-MAX_POINTS]
+        if len(to) > 2:
+            if most_recent != to[-1][1]:
+                most_recent = to[-1][1]
+                g = match(gestures, map(lambda x: x[0], to))
+                if g != None:
+                    g.execute()
+                else:
+                    print "No gesture found"
+
+def run_from_file(filename):
+    util.consume_from_file(gesture_consumer, filename)
+
+if __name__ == '__main__':
+    run_from_file(sys.argv[1])
